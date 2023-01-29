58b204fec9991a2522e9dd99199b8f9ec6fbb0a2
dessygil <desmondgilmour@gmail.com>
2023-01-29 10:52:00 -0500

    trimming down the files and making this usable again

--- alphaToe/api/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Scoreboard(models.Model):
-    user = models.CharField(max_length=50, default=generate_unique_code,unique=True)
-    computer_score = models.IntegerField(null=False, default=0)
-    user_score = models.IntegerField(null=False, default=0)
--- alphaToe/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Scoreboard
-
-class ScoreboardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Scoreboard
-        fields = ('id', 'user','computer_score','user_score')
-        
